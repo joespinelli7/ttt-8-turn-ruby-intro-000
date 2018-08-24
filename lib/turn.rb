296e6f5bbb8e82c62cbe4944120ebc9cc306af58
Joseph M Spinelli <josephspinelli@yahoo.com>
2018-08-24 14:32:36 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,7 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  if board[index] == " " | board[index] == "" | board[index] == nil
+  if array[ind] == " " | board[index] == "" | board[index] == nil
     return false
   else
     return true
